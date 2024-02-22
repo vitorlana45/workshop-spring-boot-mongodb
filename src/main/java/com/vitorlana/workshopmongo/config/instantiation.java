51b0281109241415b6f200c8296c291575717887
vitorlana45 <vitoresmerio00@gmail.com>
2024-02-21 22:06:09 -0300

    projecao dos dados do autor com DTO

--- src/main/java/com/vitorlana/workshopmongo/config/instantiation.java
+++ src/main/java/com/vitorlana/workshopmongo/config/instantiation.java
@@ -3,6 +3,7 @@ package com.vitorlana.workshopmongo.config;
 
 import com.vitorlana.workshopmongo.domain.Post;
 import com.vitorlana.workshopmongo.domain.User;
+import com.vitorlana.workshopmongo.dto.AuthorDTO;
 import com.vitorlana.workshopmongo.repository.PostRepository;
 import com.vitorlana.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,16 +25,19 @@ public class instantiation implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         userRepository.deleteAll(); // limpando a coleção dentro do mongoDB Local
+        postRepository.deleteAll();
 
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User joao = new User(null, "Joao Green", "joao@gmail.com");
         User fred = new User(null, "Fred White", "fred@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, joao, fred));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
 
