f97f0c199aadf1bc3f1165c05c7c909199df337e
vitorlana45 <vitoresmerio00@gmail.com>
2024-02-21 22:17:13 -0300

    referenciando os posts do usuario

--- src/main/java/com/vitorlana/workshopmongo/domain/User.java
+++ src/main/java/com/vitorlana/workshopmongo/domain/User.java
@@ -1,10 +1,14 @@
 package com.vitorlana.workshopmongo.domain;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -16,6 +20,8 @@ public class User implements Serializable {
     private String name;
     private String email;
 
+    @DBRef(lazy = true) // os posts so serão carregados quando eu acessá-los
+    private List<Post> posts = new ArrayList<>();
     public User() {
     }
 
@@ -47,6 +53,10 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
     }
 
     @Override
