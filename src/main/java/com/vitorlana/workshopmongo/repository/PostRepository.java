b1ce93e6020c8ac47c9f47b12441e09346320be2
vitorlana45 <vitoresmerio00@gmail.com>
2024-02-23 21:41:12 -0300

    consulta simples com @Query

--- src/main/java/com/vitorlana/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/vitorlana/workshopmongo/repository/PostRepository.java
@@ -2,14 +2,18 @@ package com.vitorlana.workshopmongo.repository;
 
 import com.vitorlana.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post,String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 
-        //Query methods Simples https://docs.spring.io/spring-data/data-document/docs/current/reference/html/#repositories.query-methods
-        List<Post> findByTitleContainingIgnoreCase(String text);
+    //Query methods Simples https://docs.spring.io/spring-data/data-document/docs/current/reference/html/#repositories.query-methods
+    List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
 }
